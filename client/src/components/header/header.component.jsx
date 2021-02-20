7ae8813502dd6d996fb2fb7e14505b91b6a61d84
kakashi8963 <mananbafna00@gmail.com>
2021-02-20 12:33:11 +0530

    final app 3

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -9,6 +9,8 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selector";
 import{ ReactComponent as Logo} from '../../assets/logo.svg';
+
+
 const Header =({currentUser,hidden}) =>(
     <div className='header'>
 <Link className='logo-container' to ="/"> <Logo/>
