1ec2075682148709a45f8d7daef57342e07e2f01
kakashi8963 <mananbafna00@gmail.com>
2021-02-20 14:09:14 +0530

    hoja pleae

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -16,7 +16,7 @@ const Header =({currentUser,hidden}) =>(
 <Link className='logo-container' to ="/"> <Logo/>
 </Link>
 <div className='options'>
-<Link className='option' to='/'>HOME </Link>
+
     <Link className='option' to='/shop'>SHOP </Link>
     
     <Link className='option' to='/contact'>CONTACT</Link>
